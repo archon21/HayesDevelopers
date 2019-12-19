007d6400b9f5711a2915f6379d54d951bdd0b904
archon21 <rwetmore3@gmail.com>
2019-12-19 00:13:57 -0500

    Functions to write heads before sending through firebase functions. React Helemet for dynamically writing title

---
+++ functions/index.js
@@ -0,0 +1,91 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+adminConfig.credential = admin.credential.cert(
+  require('./hayesdevelopers-firebase-adminsdk-ktdyy-691e103d6d.json')
+);
+adminConfig.databaseURL = 'https://hayesdevelopers.firebaseio.com';
+admin.initializeApp(adminConfig);
+// console.log('STARTED');
+
+const db = admin.firestore();
+
+//
+exports.app = functions.https.onRequest(async (req, res) => {
+  const { url } = req;
+  const urlArr = url.split('/');
+
+  if (urlArr.length >= 3) {
+    type =
+      urlArr[urlArr.length - 2] === 'development'
+        ? 'development'
+        : 'properties';
+    page = urlArr[urlArr.length - 1];
+
+    const snapshot = await db
+      .collection(type)
+      .doc(page)
+      .get();
+    const data = snapshot.data();
+    const { name, description, image } = data;
+    fillTemplate(res, name, description, image);
+  } else {
+    fillTemplate(res);
+  }
+});
+
+const fillTemplate = (res, title, desc, img) => {
+  title = title
+    ? title
+    : 'Since 1974, Hayes Developers has been a leader in real estate development, leasing and property management of retail shopping centers. Our longevity and integrity are why many companies in the New England area trust the Hayes Team with their development needs.';
+  desc = desc
+    ? desc
+    : (desc =
+        'Since 1974, Hayes Developers has been a leader in real estate development, leasing and property management of retail shopping centers. Our longevity and integrity are why many companies in the New England area trust the Hayes Team with their development needs.');
+  res.send(`<html>
+    <head>
+      <title>${title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charset="UTF-8" />
+      <meta
+        name="description"
+        content="${desc}"
+      />
+      <meta
+          name="keywords"
+          content="${desc.split(' ').join(',')}"
+      />
+      <meta property="og:title" content="${title}">
+      ${img && `<meta property="og:image" content="${img}">`}
+      <meta property="og:description" content="${desc}">
+      <meta
+        name="google-site-verification"
+        content="rm0m4J1fq_w04P1fjIC62KG2snIJhDvaNUlT7VN5lUY"
+      />
+
+      <link rel="stylesheet" href="/js/css/style.css" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Oxygen|Montserrat|Questrial"
+        rel="stylesheet"
+      />
+      <link
+        rel="icon"
+        href="https://firebasestorage.googleapis.com/v0/b/hayesdevelopers.appspot.com/o/Logos%2Flogo.png?alt=media&token=b8dff8c7-dafa-41cd-838c-a4b544a8a70c"
+      />
+      <script defer src="/js/bundle.js"></script>
+    </head>
+
+    <body>
+      <div id="app"></div>
+    </body>
+  </html>
+  `);
+};
